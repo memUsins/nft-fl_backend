82be20874c2f4977271ee11ee7b8c00432216335
Mem U Sins <83538724+memUsins@users.noreply.github.com>
2022-07-20 00:54:21 +0300

    fix

--- src/utils/mysqlConnect.js
+++ src/utils/mysqlConnect.js
@@ -1,14 +1,21 @@
 import mysql from "mysql";
 import config from "./config.js";
+let connection
+try {
+    connection = mysql.createPool({
+        host: config.db_host,
+        user: config.db_login,
+        password: config.db_pass,
+        database: config.db_table,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    });
+} catch (err) {
+    connection = null;
+    console.log("Database is droped", err)
+    process.exit(1)
+}
 
-const connection = mysql.createPool({
-    host: config.db_host,
-    user: config.db_login,
-    password: config.db_pass,
-    database: config.db_table,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
 
 export default connection;
