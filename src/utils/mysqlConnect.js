f358d59296dd0c469156c9a6d739ebb84e9089c7
Mem U Sins <83538724+memUsins@users.noreply.github.com>
2022-07-20 01:06:47 +0300

    fix

--- src/utils/mysqlConnect.js
+++ src/utils/mysqlConnect.js
@@ -1,8 +1,10 @@
 import mysql from "mysql";
 import config from "./config.js";
-let connection
-try {
-    connection = mysql.createPool({
+
+console.log(123)
+
+export default new Promise((resolve, reject) => {
+    let pool = mysql.createPool({
         host: config.db_host,
         user: config.db_login,
         password: config.db_pass,
@@ -11,11 +13,23 @@ try {
         connectionLimit: 10,
         queueLimit: 0
     });
-} catch (err) {
-    connection = null;
-    console.log("Database is droped", err)
-    process.exit(1)
-}
 
+    pool.getConnection((err, con) => {
+        if (err) {
+            console.log("DB is not connected", err);
+            process.exit(1)
+        }
+        try {
+            if (con) {
+                con.release();
+                console.log("DB is connected")
+                resolve(pool);
+            }
+        } catch (err) {
+            console.log("MySQL error", err);
+            process.exit(1)
+        }
 
-export default connection;
+    });
+});
+console.log(1233)
